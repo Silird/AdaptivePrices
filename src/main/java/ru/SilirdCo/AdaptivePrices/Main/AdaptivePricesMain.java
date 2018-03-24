83c556773c0c23579fa5003f0229759313957b36
Silird <silird@gmail.com>
2018-03-24 22:09:59 +0300

    init, some view of javaFX, standart maven settings, logs, some standart utils

---
+++ src/main/java/ru/SilirdCo/AdaptivePrices/Main/AdaptivePricesMain.java
@@ -0,0 +1,9 @@
+package ru.SilirdCo.AdaptivePrices.Main;
+
+import ru.SilirdCo.AdaptivePrices.View.Launch.LaunchView;
+
+public class AdaptivePricesMain {
+    public static void main(String[] args) {
+        LaunchView.launch();
+    }
+}
---
+++ src/main/java/ru/SilirdCo/AdaptivePrices/View/Launch/LaunchView.java
@@ -0,0 +1,25 @@
+package ru.SilirdCo.AdaptivePrices.View.Launch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.SilirdCo.AdaptivePrices.Util.ExceptionHandler;
+
+public class LaunchView {
+    private static final Logger logger = LoggerFactory.getLogger(LaunchView.class);
+
+    public static boolean launch() {
+        logger.info("\n\nЗапуск визуальной формы\n\n");
+
+        try {
+            logger.info("\n\nЗапуск формы..\n\n");
+            MainJavaFX.show(new String[0]);
+        }
+        catch (Exception ex) {
+            ExceptionHandler.handle(logger, ex);
+
+            return false;
+        }
+
+        return true;
+    }
+}
